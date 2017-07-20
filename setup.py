3ed5ccf13c04725f5ff7a0b943951cd66e4c25f0
Joshua Clayton <stillcompiling@gmail.com>
2017-07-20 13:29:36 -0700

    Fix setup.py basics
    Correct the package name
    Add author middle initial, add license and homepage

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='buildbot-gogs',
+setup(name='buildbot-webhook-gogs',
       version='0.1',
       description='Gogs web hook support for buildbot 9.9 and above',
-      author='Joshua Clayton',
+      author='Joshua A Clayton',
       author_email='stillcompiling@gmail.com',
-      packages=['buildbot_gogs',],
+      url='https://github.com/d4ddi0/buildbot-webhook-gogs',
+      license='GPLv2',
+      packages=['buildbot_webhook_gogs',],
       entry_points={'buildbot.webhooks': 'gogs = buildbot_webhook_gogs:gogs'}
 )
 
